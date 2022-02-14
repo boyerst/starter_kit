6e0659eddab8b1304fb42a67ce1ff0ec75fbb73a
Steve Boyer <stephen.a.boyer@gmail.com>
2022-02-13 18:53:00 -0600

    Deploy contract to ganache

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,15 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1000 DAI tokens for deployer
+  await tokenMock.mint(
+    '0xfa7A3d6eC5Bc1e722aF8Ee6c5ad227a708907Ae1',
+    // Since we are minting 1000 tokens and our token has 18 decimals, we list out 1000 and add 18 decimals
+    '1000000000000000000000'
+  )
 };
